f6acfc14c36dc87a97c8c6e95862aa71be929843
Kaila Sprague McRae <kaila.sprague@icloud.com>
2021-01-19 11:44:54 -0800

    add validations to Animal class

--- CretaceousPark/Models/CretaceousParkContext.cs
+++ CretaceousPark/Models/CretaceousParkContext.cs
@@ -12,7 +12,7 @@ namespace CretaceousPark.Models
         public DbSet<Animal> Animals { get; set; }
 
         protected override void OnModelCreating(ModelBuilder builder)
-{
+        {
         builder.Entity<Animal>()
             .HasData(
                 new Animal { AnimalId = 1, Name = "Matilda", Species = "Woolly Mammoth", Age = 7, Gender = "Female" },
@@ -21,6 +21,6 @@ namespace CretaceousPark.Models
                 new Animal { AnimalId = 4, Name = "Pip", Species = "Shark", Age = 4, Gender = "Male" },
                 new Animal { AnimalId = 5, Name = "Bartholomew", Species = "Dinosaur", Age = 22, Gender = "Male" }
             );
-}
+        }
     }
 }
